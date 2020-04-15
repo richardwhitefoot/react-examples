9b98449ef9bd02582cfdade39768d90229bde271
Richard Whitefoot <mouseholdonline@gmail.com>
2020-04-15 12:52:02 +0100

    fix: Remove config
    Had been reset

--- shopproject/src/firebase/firebase.utils.js
+++ shopproject/src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCTfUS55HMLOE8N5i8gpzqxTaoL6NZ1_ps",
-    authDomain: "shoppoc-408bf.firebaseapp.com",
-    databaseURL: "https://shoppoc-408bf.firebaseio.com",
-    projectId: "shoppoc-408bf",
-    storageBucket: "shoppoc-408bf.appspot.com",
-    messagingSenderId: "447467877646",
-    appId: "1:447467877646:web:741855e6f74c3cdef4535f"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 
 firebase.initializeApp(config)
